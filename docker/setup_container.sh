22ca6a01426c4778c6bc70e0295cc1be494cf1cc
Martin Hansson <martinhson@gmail.com>
2026-01-16 13:41:06 +0100

    Packages needed for MTR, fixes

--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -4,9 +4,13 @@ image_name=${1:-dev}
 container_name=${2:-mydev}
 username=${3:-martin}
 
-docker run -d --name "$container_name" -v "$HOME/gitroot:/home/$username/gitroot" \
+home="/home/$username"
+
+docker run -d --name "$container_name" -v "$HOME/gitroot:$home/gitroot" \
        --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null && \
-docker cp ~/.ssh/id_rsa "$container_name:/home/$username/.ssh/id_rsa" && \
-docker cp ~/.ssh/id_rsa.pub "$container_name:/home/$username/.ssh/id_rsa.pub" && \
-docker exec -uroot "$container_name" chown "$username:$username" "/home/$username/.ssh/id_rsa" && \
-docker exec -uroot "$container_name" chown "$username:$username" "/home/$username/.ssh/id_rsa.pub"
+docker cp ~/.ssh/id_rsa "$container_name:$home/.ssh/id_rsa" && \
+docker cp ~/.ssh/id_rsa.pub "$container_name:$home/.ssh/id_rsa.pub" && \
+docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa" && \
+docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa.pub"
+# Personally, I keep this file in Git and just symlink it
+docker -L cp ~/.gitconfig "${container_name}:$home/"

6cbdca5e7cba682d8b85993e631653b97dc98c1c
Martin Hansson <martinhson@gmail.com>
2026-02-06 10:30:35 +0100

    Stopped hard-coding my username

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 image_name=${1:-dev}
-username=${2:-martin}
+username=${2:-$(whoami)}
+
+echo "Building image '$image_name' for user '$username'"
 
 docker build . -t "$image_name" --build-arg username="$username"
--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -2,15 +2,19 @@
 
 image_name=${1:-dev}
 container_name=${2:-mydev}
-username=${3:-martin}
+username=${3:-$(whoami)}
 
 home="/home/$username"
 
-docker run -d --name "$container_name" -v "$HOME/gitroot:$home/gitroot" \
+echo "Setting up container '$container_name' from image '$image_name' for user '$username'"
+
+docker run -d --name "$container_name" \
+       -v "$HOME/gitroot:$home/gitroot" \
+       -v "$HOME/boost:$home/boost" \
        --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null && \
 docker cp ~/.ssh/id_rsa "$container_name:$home/.ssh/id_rsa" && \
 docker cp ~/.ssh/id_rsa.pub "$container_name:$home/.ssh/id_rsa.pub" && \
 docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa" && \
 docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa.pub"
 # Personally, I keep this file in Git and just symlink it
-docker -L cp ~/.gitconfig "${container_name}:$home/"
+docker cp -L ~/.gitconfig "${container_name}:$home/"

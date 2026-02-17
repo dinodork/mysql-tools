53b2c8a68c7c88ec3da18088db18ac245e7d7cc3
Martin Hansson <martinhson@gmail.com>
2026-02-17 08:53:09 +0000

    Fixed a Heisenbug: mysql-test-run wouldn't start and was hanging indefinitely. When I added a statement to print the commandline beforehand, it started working. Move on. *shrugs*

--- mytr.py
+++ mytr.py
@@ -1,15 +1,28 @@
 """Runs mysql-test-run from the root of the git clone"""
 
-import os
-import sys
-import subprocess
 from shutil import which
+import argparse
+import os
+import subprocess
+import sys
 
 import mysql
 
 
 def main():
     """All the work"""
+
+    parser = argparse.ArgumentParser(description="Starts mysqld in the background.")
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        help="verbose",
+    )
+
+    args, mtr_args = parser.parse_known_args()
 
     cwd = os.path.abspath(
         f"{mysql.Defaults.BUILD_HOME}/{mysql.Defaults.BUILD_TYPE}/mysql-test"
@@ -19,19 +32,21 @@ def main():
     mtr_args = [exe] + sys.argv[1:]
 
     # If `colordiff` is available, pipe the test-run output through it.
+    if args.verbose:
+        print(f"Running mysql-test-run like this: {" ".join(mtr_args)}")
     if which("colordiff"):
         with subprocess.Popen(
             mtr_args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        ) as p:
-            with subprocess.Popen(["colordiff"], stdin=p.stdout) as cd:
+        ) as mtr_proc:
+            with subprocess.Popen(["colordiff"], stdin=mtr_proc.stdout) as cd:
                 # Allow p to receive a SIGPIPE if colordiff exits.
-                p.stdout.close()
+                mtr_proc.stdout.close()
                 cd.wait()
                 rc = cd.returncode
     else:
-        with subprocess.Popen(mtr_args, cwd=cwd) as p:
-            p.wait()
-            rc = p.returncode
+        with subprocess.Popen(mtr_args, cwd=cwd) as mtr_proc:
+            mtr_proc.wait()
+            rc = mtr_proc.returncode
 
     sys.exit(rc)
 

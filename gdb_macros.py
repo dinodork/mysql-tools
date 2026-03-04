a719b6c0b4d7c3a3e0ead086ac886d9635cb254d
Martin Hansson <martinhson@gmail.com>
2026-03-04 10:00:40 +0100

    Starting with GDB macros

---
+++ gdb_macros.py
@@ -0,0 +1,19 @@
+import gdb
+
+
+class KeyPrinter(gdb.Command):
+    """Prints Hello, World!"""
+
+    def __init__(self):
+        super(KeyPrinter, self).__init__("pkey", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        val = gdb.parse_and_eval(arg)
+        if val[0]:
+            print("string: NULL")
+        else:
+            print(f"debug {val[1]} {val[2]}")
+            print(f"string of length {val[1] + (int(val[2]) << 16)}:{val + 3}")
+
+
+KeyPrinter()

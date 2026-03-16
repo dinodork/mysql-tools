6652e1490fc1a61e425869ea5866f24ddd3f00c2
Martin Hansson <martinhson@gmail.com>
2026-03-16 13:40:04 +0000

    Separated out ASAN and clang

--- build-trunk.sh
+++ build-trunk.sh
@@ -4,17 +4,21 @@ build_type=${1:-Debug}
 build_dir=${2:-build/${build_type,,}}
 
 if [ "$clang" == "1" ]; then
-   clang="-DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++-18"
+   clang_opt="-DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++-18"
    build_dir="$build_dir.clang"
+fi
+
+if [ "$asan" == 1 ]; then
+   asan_opt="-DWITH_ASAN=ON"
 fi
 
 cmd="cmake -B ${build_dir} -GNinja -DCMAKE_BUILD_TYPE=$build_type \
      -DDOWNLOAD_BOOST=1 -DWITH_BOOST=~/boost \
      -DWITH_AUTHENTICATION_LDAP=OFF -DWITH_PERCONA_AUTHENTICATION_LDAP=OFF \
-     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DFORCE_COLORED_OUTPUT=ON"
+     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DFORCE_COLORED_OUTPUT=ON \
+     $clang_opt \
+     $asan_opt"
 
 echo $cmd
 
-${cmd}
-
-time ninja -C "$build_dir"
+${cmd} && time ninja -C "$build_dir"

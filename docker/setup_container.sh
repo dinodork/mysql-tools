e2012b9ebe5dcc502c4514846eac80a0940a2425
Martin Hansson <martinhson@gmail.com>
2026-03-17 16:54:36 +0100

    debuginfod

--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -13,10 +13,12 @@ docker run -d --name "$container_name" \
        -v "$HOME/boost:$home/boost" \
        --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null && \
 docker cp ~/.ssh/id_rsa "$container_name:$home/.ssh/id_rsa" && \
-docker cp ~/.ssh/id_rsa.pub "$container_name:$home/.ssh/id_rsa.pub" && \
-docker cp .bash_aliases "$container_name:$home/" && \
-docker cp .gdbinit "$container_name:$home/" && \
 docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa" && \
+docker cp ~/.ssh/id_rsa.pub "$container_name:$home/.ssh/id_rsa.pub" && \
 docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa.pub"
+docker cp .bash_aliases "$container_name:$home/" && \
+docker exec -uroot "$container_name" chown "$username:$username" "$home/.bash_aliases"
+docker cp .gdbinit "$container_name:$home/" && \
+docker exec -uroot "$container_name" chown "$username:$username" "$home/.gdbinit"
 # Personally, I keep this file in Git and just symlink it
 docker cp -L ~/.gitconfig "${container_name}:$home/"

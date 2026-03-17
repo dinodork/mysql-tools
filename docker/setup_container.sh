a97a57672252e9d61d4584b9dbdbd5847a229de0
Martin Hansson <martinhson@gmail.com>
2026-03-17 16:55:14 +0100

    Fool-proofed container setup

--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -8,17 +8,24 @@ home="/home/$username"
 
 echo "Setting up container '$container_name' from image '$image_name' for user '$username'"
 
+copy_file_to_container() {
+  source=$1
+  target=${home}/$(basename $1)
+  docker cp -L "${source}" "${container_name}:$target"
+  docker exec -uroot "$container_name" chown "$username:$username" "$target"
+}
+
+copy_files_to_container() {
+  files=$@
+  for file in $files; do
+    copy_file_to_container "$file"
+  done
+}
+
 docker run -d --name "$container_name" \
        -v "$HOME/gitroot:$home/gitroot" \
        -v "$HOME/boost:$home/boost" \
        --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null && \
-docker cp ~/.ssh/id_rsa "$container_name:$home/.ssh/id_rsa" && \
-docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa" && \
-docker cp ~/.ssh/id_rsa.pub "$container_name:$home/.ssh/id_rsa.pub" && \
-docker exec -uroot "$container_name" chown "$username:$username" "$home/.ssh/id_rsa.pub"
-docker cp .bash_aliases "$container_name:$home/" && \
-docker exec -uroot "$container_name" chown "$username:$username" "$home/.bash_aliases"
-docker cp .gdbinit "$container_name:$home/" && \
-docker exec -uroot "$container_name" chown "$username:$username" "$home/.gdbinit"
+copy_files_to_container $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa.pub .bash_aliases .gdbinit && \
 # Personally, I keep this file in Git and just symlink it
-docker cp -L ~/.gitconfig "${container_name}:$home/"
+copy_file_to_container "$HOME/.gitconfig"

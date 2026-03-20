cee437cf8cea7953313851d2b3a944fa97ef8461
Martin Hansson <martinhson@gmail.com>
2026-03-20 10:32:58 +0100

    Fixed broken Docker scripts

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,7 +2,8 @@
 
 image_name=${1:-dev}
 username=${2:-$(whoami)}
+ssh_key=id_rsa
 
 echo "Building image '$image_name' for user '$username'"
 
-docker build . -t "$image_name" --build-arg username="$username"
+docker build . -t "$image_name" --build-arg username="$username" --build-arg keyfile="$ssh_key"
--- docker/setup_container.sh
+++ docker/setup_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 image_name=${1:-dev}
 container_name=${2:-mydev}
 username=${3:-$(whoami)}
@@ -8,24 +10,36 @@ home="/home/$username"
 
 echo "Setting up container '$container_name' from image '$image_name' for user '$username'"
 
-copy_file_to_container() {
+
+copy_file() {
   source=$1
-  target=${home}/$(basename $1)
-  docker cp -L "${source}" "${container_name}:$target"
+  target=$2
+  docker cp -L "${source}" "${container_name}:$target" -q
   docker exec -uroot "$container_name" chown "$username:$username" "$target"
 }
 
-copy_files_to_container() {
+copy_file_to_home() {
+  source=$1
+  target=${home}/$(basename $1)
+  docker cp -L "${source}" "${container_name}:$target" -q
+  docker exec -uroot "$container_name" chown "$username:$username" "$target"
+}
+
+copy_files_to_home() {
   files=$@
   for file in $files; do
-    copy_file_to_container "$file"
+    copy_file_to_home "$file"
   done
 }
 
 docker run -d --name "$container_name" \
        -v "$HOME/gitroot:$home/gitroot" \
        -v "$HOME/boost:$home/boost" \
-       --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null && \
-copy_files_to_container $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa.pub .bash_aliases .gdbinit && \
+       --cap-add=SYS_PTRACE "$image_name" tail -f /dev/null
+
+copy_file $HOME/.ssh/id_rsa ${home}/.ssh/id_rsa
+copy_file $HOME/.ssh/id_rsa.pub ${home}/.ssh/id_rsa.pub
+copy_files_to_home .bash_aliases .gdbinit
+
 # Personally, I keep this file in Git and just symlink it
-copy_file_to_container "$HOME/.gitconfig"
+copy_file_to_home "$HOME/.gitconfig"
